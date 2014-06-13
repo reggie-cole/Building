441b78508c03f042e69b21738eae806b2417e0e8
regianld <regianld@192.168.1.134>
2014-06-13 18:10:28 -0500

    Connected a View

--- hello_building/src/main/java/hello/controllers/HelloController.java
+++ hello_building/src/main/java/hello/controllers/HelloController.java
@@ -1,14 +1,17 @@
 package hello.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RestController
+@Controller
 public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot!";
+        return "greeting";
     }
     
 }
